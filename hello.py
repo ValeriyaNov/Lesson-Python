a8b596500f5437fbb343ec81b85a4005032595c9
Valeriya <wal.now@yandex.ru>
2022-12-04 15:42:17 +0300

    1

---
+++ hello.py
@@ -0,0 +1,57 @@
+# print('hello \nword')
+a=223
+b=1.5
+# print(type(a))
+# print(b)
+
+value=None
+value=1233
+# print(type(value))
+
+s='yellow'
+
+
+# print(a,'-', b,'-', s)
+# print('{1} - {2} - {0}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+
+# f = True
+# print(f)
+
+# list = [1, 2, 3]
+# print(list)
+
+# print('Введите а')
+# a=int(input())
+# print('Введите b')
+# b=int(input())
+# print(a, '+', b, '=', a+b)
+
+# a=2
+# b=8
+# c=a+b
+# s=a**b
+# print(s)
+
+# a=1<4 and 5>2
+# b=1<3<5<10
+# print(b)
+# f=[1,2,3,4]
+# print(not 2 in f)
+
+# is_odd=f[0]%2==0
+
+
+# a=int(input('a= '))
+# b=int(input('b= '))
+# if a>b:
+#     print(a)
+# else:
+#     print(b)
+
+# for i in 1,2,3,4,5:
+#     print(i**2)
+
+r=range(10)
+for i in r:
+    print(i)
